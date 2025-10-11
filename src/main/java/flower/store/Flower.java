a7395082d1bc904103b6ab11558fed5a4c00d66e
Yelizaveta Piletska <piletska.pn@ucu.edu.ua>
2025-10-11 23:20:30 +0300

    Update Flower.java

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,1 +1,37 @@
+package flower.store;
 
+public class Flower {
+    private double sepalLength;
+    private FlowerColor color;
+    private double price;
+
+
+    public double getSepalLength() {
+        return sepalLength;
+    }
+
+
+    public void setSepalLength(final double newSepalLength) {
+        this.sepalLength = newSepalLength;
+    }
+
+
+    public String getColor() {
+        return color.getColorString();
+    }
+
+  
+    public void setColor(final FlowerColor newColor) {
+        this.color = newColor;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(final double newPrice) {
+        this.price = newPrice;
+    }
+}
