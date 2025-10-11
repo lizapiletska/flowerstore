4bc39f94cfecbe7a1cae5c68ed7aec043a61cf8c
Yelizaveta Piletska <piletska.pn@ucu.edu.ua>
2025-10-11 23:24:27 +0300

    Create FlowerBucket.java

---
+++ src/main/java/flower/store/FlowerBucket.java
@@ -0,0 +1,23 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FlowerBucket {
+    private List<FlowerPack> flowerPacks = new ArrayList<>();
+
+
+    public void add(final FlowerPack flowerPack) {
+        flowerPacks.add(flowerPack);
+    }
+
+
+    public double getPrice() {
+        double totalPrice = 0.0;
+        for (FlowerPack pack : flowerPacks) {
+            totalPrice += pack.getPrice();
+        }
+        return totalPrice;
+    }
+}
